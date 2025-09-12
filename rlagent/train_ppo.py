485c211434eea5901475dc0b8113388d433cbd31
suvrat-jain-git <suvrat.bs24dmu007@spjain.org>
2025-09-12 16:25:25 +1000

    venv resolved

--- rlagent/train_ppo.py
+++ rlagent/train_ppo.py
@@ -9,18 +9,18 @@ def main():
         env=env,
         verbose=1,
         tensorboard_log="./ppo_tensorboard/",
-        learning_rate=3e-4,   
+        learning_rate=3e-5,   
         n_steps=2048,         
-        batch_size=64,        
+        batch_size=1024,        
         gamma=0.99,           
-        clip_range=0.2
+        clip_range=0.15
     )
     total_timesteps = 100000
     model.learn(total_timesteps=total_timesteps)
 
-    model.save("ppo_carlos_agent")
+    model.save("tuned_ppo_carlos_agent")
 
-    print("Training complete. Model saved as ppo_carlos_agent.zip")
+    print("Training complete. Model saved as tuned_ppo_carlos_agent.zip")
 
 if __name__ == "__main__":
     main()
