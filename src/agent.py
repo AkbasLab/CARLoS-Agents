e76f84c5d31cfa7da0f59a04bb5b79c7d346350a
suvrat-jain-git <suvrat.bs24dmu007@spjain.org>
2026-03-19 21:07:19 +1100

    Final Update 6

--- src/agent.py
+++ src/agent.py
@@ -51,3 +51,23 @@ class SimpleAgent(Agent):
 
     def compute_reward(self, state, in_lane: bool, in_motion: bool) -> float:
         return 1.0 if in_lane and in_motion else -1.0
+
+ 
+    def compute_reward(self, state, in_lane: bool, in_motion: bool, collision=False, weights=None) -> float:
+        if weights is None:
+            weights = {
+                'in_lane': 1.0,
+                'in_motion': 0.5,
+                'collision': -50.0,
+                'out_lane': -10.0
+            }
+
+        reward = 0.0
+
+        if collision:
+            reward += weights['collision']
+        else:
+            reward += weights['in_lane'] if in_lane else weights['out_lane']
+            reward += weights['in_motion'] if in_motion else 0.0
+
+        return reward
--- src/presentation_agent.py
+++ src/presentation_agent.py
@@ -13,5 +13,22 @@ class PresentationAgent(Agent):
         steering = self.sensors.sensors[index].angle_offset
         return steering, accel
 
-    def compute_reward(self, state, in_lane: bool, in_motion: bool) -> float:
-        return 1.0 if in_lane and in_motion else -1.0
+    def compute_reward(self, state, in_lane: bool, in_motion: bool, collision=False, weights=None) -> float:
+        if weights is None:
+            weights = {
+            'in_lane': 1.0,
+            'in_motion': 0.5,
+            'collision': -50.0,
+            'out_lane': -10.0
+            }
+
+        reward = 0.0
+
+        if collision:
+            reward += weights['collision']
+        else:
+            reward += weights['in_lane'] if in_lane else weights['out_lane']
+            reward += weights['in_motion'] if in_motion else 0.0
+
+        return reward
+
